5fb9dcc39eb8bbe39f3afe806912a6ed4ed80e16
brownas98 <46372392+brownas98@users.noreply.github.com>
2019-05-20 09:18:12 +0100

    included Quantity List in the Main app example

--- supermarket-pricing/src/main/java/supermarket/App.java
+++ supermarket-pricing/src/main/java/supermarket/App.java
@@ -7,9 +7,6 @@ import java.util.Map;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hello World");
 		
 		Map<String, Product> productDictionary = new HashMap<>();
 		
@@ -20,11 +17,12 @@ public class App {
 		Map<String, ProductDiscount> productDiscountDictionary = new HashMap<>();
 		
 		productDiscountDictionary.put("Beans", new ProductDiscount("Beans 3 for 2", BigDecimal.valueOf(-50), 3));
-		productDiscountDictionary.put("Coke", new ProductDiscount("Coke 2 for £1", BigDecimal.valueOf(-40), 2));
+		productDiscountDictionary.put("Coke", new ProductDiscount("Coke 2 for Â£1", BigDecimal.valueOf(-40), 2));
 		
 		Basket basket1 = new Basket( new ProductOrderCalculator(productDictionary, productDiscountDictionary));
 		ProductOrder productOrder1 = new ProductOrder("Beans", 3);
 		ProductOrder productOrder2 = new ProductOrder("Coke", 2);
+		//weight calculated items are input using grams. eg 0.2 kg = 200 
 		ProductOrder productOrder3 = new ProductOrder("Oranges", 200);
 		basket1.add(productOrder1);
 		basket1.add(productOrder2);
@@ -39,7 +37,12 @@ public class App {
 		System.out.println("");
 		System.out.println("Discount List");
 		System.out.println("Beans 3 for 2");
-		System.out.println("Coke 2 for £1");
+		System.out.println("Coke 2 for Â£1");
+		System.out.println("");
+		System.out.println("Quantity List");
+		System.out.println("Beans X 3");
+		System.out.println("Coke X 2");
+		System.out.println("Oranges X 200 grams");
 		System.out.println("");
 		System.out.println("Sub Total " + basket1.getSubTotalPounds());
 		System.out.println("Discount Total " + basket1.getDiscountTotalPounds());
